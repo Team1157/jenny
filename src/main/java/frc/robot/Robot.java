bc2359999a2d64991d3ccde8e0027a5cefd1028b
ada <git@adas.software>
2024-10-29 23:37:44 -0600

    field relative shopping cart. no more description required

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,18 +1,14 @@
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends TimedRobot {
   private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
   private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
@@ -20,57 +16,61 @@ public class Robot extends TimedRobot {
       new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
+  double kP = 0.05;
+  ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
   public Robot() {
     SendableRegistry.addChild(m_robotDrive, m_leftDrive);
     SendableRegistry.addChild(m_robotDrive, m_rightDrive);
   }
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
     m_rightDrive.setInverted(false);
+    gyro.calibrate(); // Calibrate gyro on startup
+
+    // Add a toggle to the dashboard for selecting field-relative or robot-relative control
+    SmartDashboard.putBoolean("Field-Relative Drive", true); // Default to field-relative
   }
 
-  /** This function is run once each time the robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
     m_timer.restart();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    // Drive for 2 seconds
     if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
       m_robotDrive.arcadeDrive(0.5, 0.0, false);
     } else {
-      m_robotDrive.stopMotor(); // stop robot
+      m_robotDrive.stopMotor();
     }
   }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRawAxis(5));
+    // Get the joystick inputs
+    double xSpeed = -m_controller.getLeftY(); // Forward/Backward
+    double ySpeed = m_controller.getLeftX(); // Left/Right
+
+    // Check if field-relative mode is enabled
+    boolean isFieldRelative = SmartDashboard.getBoolean("Field-Relative Drive", true);
+
+    if (isFieldRelative) {
+      // Calculate the field-relative angle
+      double gyroAngle = Math.toRadians(gyro.getAngle()); // Convert to radians
+      double temp = xSpeed * Math.cos(gyroAngle) + ySpeed * Math.sin(gyroAngle);
+      ySpeed = -xSpeed * Math.sin(gyroAngle) + ySpeed * Math.cos(gyroAngle);
+      xSpeed = temp;
+    }
+
+    // Use arcadeDrive with the transformed inputs if field-relative, or raw if robot-relative
+    m_robotDrive.arcadeDrive(xSpeed, ySpeed);
   }
 
-  /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
